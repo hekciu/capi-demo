7529f5aeffebb14e39fac2a498f97fd526b538b9
Jakub Lasecki <jakublasecki321@gmail.com>
2024-09-01 17:43:20 +0000

    indentation

--- socket.c
+++ socket.c
@@ -34,25 +34,25 @@ void * handle_client(void * arg){
 	ssize_t bytes_received = recv(client_fd, buffer, BUFFER_SIZE, 0);
 
 	if (bytes_received > 0) {
-		regex_t regex;
-		regcomp(&regex, "^GET /([^ ]*) HTTP/1", REG_EXTENDED);
-		regmatch_t matches[2];
+        regex_t regex;
+        regcomp(&regex, "^GET /([^ ]*) HTTP/1", REG_EXTENDED);
+        regmatch_t matches[2];
 
-		int reti = regexec(&regex, buffer, 2, matches, 0);
+        int reti = regexec(&regex, buffer, 2, matches, 0);
 
         printf("got something\n");
-		if (reti == 0) {
+        if (reti == 0) {
 			char * response = NULL;
             printf("got http request\n");
             size_t responseSize = build_http_response(&response);
-			send(client_fd, response, responseSize, 0);
+            send(client_fd, response, responseSize, 0);
             if (response != NULL) {
                 free(response);
             }
             close(client_fd);
-		}
+        }
 
-		regfree(&regex);
+        regfree(&regex);
 	}
 	
 	free(buffer);
