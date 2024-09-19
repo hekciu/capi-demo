5102b586e44e22cdc46066072989d0a9b276ffcb
hekciu <jakublasecki321@gmail.com>
2024-09-19 22:10:19 +0200

    did a bit of working postgres

--- postgres.c
+++ postgres.c
@@ -12,7 +12,7 @@ int main(int argc, char ** argv){
     const char * conninfo;
     PGconn * conn;
     PGresult * res;
-    int nFields, i, j;
+    int nTuples, nFields;
 
     if (argc < 2) {
         printf("You need to provide connection info as a first argument\n"); 
@@ -29,8 +29,25 @@ int main(int argc, char ** argv){
     }
 
     printf("success!\n"); 
-    exit_nicely(conn);
-    // res = PQexec(conn, "SELECT * FROM " 
 
+    res = PQexec(conn, "SELECT * FROM pms5003_measurements");
+    ExecStatusType execStatus = PQresultStatus(res);
+    printf("ExecStatusType: %d\n", execStatus);
+    if (execStatus == PGRES_COMMAND_OK) {
+        printf("Command OK, got no data\n");
+        exit_nicely(conn);
+    } else if (execStatus == PGRES_TUPLES_OK) {
+        printf("Command OK, got some pointer ass data\n");
+    } else if (execStatus == PGRES_SINGLE_TUPLE) {
+        printf("Command OK, got single tuple\n");
+    } else {
+        fprintf(stderr, "Something went wrong"); 
+        exit_nicely(conn);
+    } 
+    nTuples = PQntuples(res); 
+    nFields = PQnfields(res);
+    printf("n rows: %d, n columns: %d", nTuples, nFields);
+
+    exit_nicely(conn);
     return 0; 
 }
